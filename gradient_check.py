2e8b0032621400d69555cd0e8d283c8177cc7b3a
Lê Tùng Lâm <78714490+TuLa-1031@users.noreply.github.com>
2025-03-08 13:57:04 +0700

    Update gradient_check.py

--- gradient_check.py
+++ gradient_check.py
@@ -7,41 +7,27 @@ from random import randrange
 
 
 def eval_numerical_gradient(f, x, verbose=True, h=0.00001):
-    """
-    a naive implementation of numerical gradient of f at x
-    - f should be a function that takes a single argument
-    - x is the point (numpy array) to evaluate the gradient at
-    """
-
-    fx = f(x)  # evaluate function value at original point
+    fx = f(x)
     grad = np.zeros_like(x)
-    # iterate over all indexes in x
     it = np.nditer(x, flags=["multi_index"], op_flags=["readwrite"])
     while not it.finished:
-
-        # evaluate function at x+h
         ix = it.multi_index
         oldval = x[ix]
-        x[ix] = oldval + h  # increment by h
-        fxph = f(x)  # evalute f(x + h)
+        x[ix] = oldval + h 
+        fxph = f(x)
         x[ix] = oldval - h
-        fxmh = f(x)  # evaluate f(x - h)
-        x[ix] = oldval  # restore
+        fxmh = f(x)
+        x[ix] = oldval
 
-        # compute the partial derivative with centered formula
-        grad[ix] = (fxph - fxmh) / (2 * h)  # the slope
+        grad[ix] = (fxph - fxmh) / (2 * h)
         if verbose:
             print(ix, grad[ix])
-        it.iternext()  # step to next dimension
+        it.iternext()
 
     return grad
 
 
 def eval_numerical_gradient_array(f, x, df, h=1e-5):
-    """
-    Evaluate a numeric gradient for a function that accepts a numpy
-    array and returns a numpy array.
-    """
     grad = np.zeros_like(x)
     it = np.nditer(x, flags=["multi_index"], op_flags=["readwrite"])
     while not it.finished:
@@ -60,23 +46,6 @@ def eval_numerical_gradient_array(f, x, df, h=1e-5):
 
 
 def eval_numerical_gradient_blobs(f, inputs, output, h=1e-5):
-    """
-    Compute numeric gradients for a function that operates on input
-    and output blobs.
-
-    We assume that f accepts several input blobs as arguments, followed by a
-    blob where outputs will be written. For example, f might be called like:
-
-    f(x, w, out)
-
-    where x and w are input Blobs, and the result of f will be written to out.
-
-    Inputs:
-    - f: function
-    - inputs: tuple of input blobs
-    - output: output blob
-    - h: step size
-    """
     numeric_diffs = []
     for input_blob in inputs:
         diff = np.zeros_like(input_blob.diffs)
@@ -116,11 +85,11 @@ def grad_check_sparse(f, x, analytic_grad, num_checks=10, h=1e-5):
         ix = tuple([randrange(m) for m in x.shape])
 
         oldval = x[ix]
-        x[ix] = oldval + h  # increment by h
-        fxph = f(x)  # evaluate f(x + h)
-        x[ix] = oldval - h  # increment by h
-        fxmh = f(x)  # evaluate f(x - h)
-        x[ix] = oldval  # reset
+        x[ix] = oldval + h 
+        fxph = f(x) 
+        x[ix] = oldval - h 
+        fxmh = f(x) 
+        x[ix] = oldval
 
         grad_numerical = (fxph - fxmh) / (2 * h)
         grad_analytic = analytic_grad[ix]
